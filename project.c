c88e12a3c618389222215c7f983f3c7770cee44d
Jakub Skurcak <skurcak@outlook.com>
2019-11-24 20:02:08 +0100

    Create histogram function

--- project.c
+++ project.c
@@ -6,7 +6,7 @@ int edit(int original[], int edited[]);
 void print_original( int original[]);
 void print_edited(int edited[]);
 int unencrypt(int edited[]);
-int print_histogram(int edited[]);
+void print_histogram(int edited[]);
 void main(){
     int original[1000],edited[1000],control_command,i;
     for(i=0;i<1000;++i){
@@ -26,7 +26,7 @@ void main(){
 			break;
 		case 'd':
 			break;
-		case 'h': 
+		case 'h': print_histogram(edited);
 			break;
 		case 'c': edited = unencrypt(edited);
 			break;
@@ -134,8 +134,156 @@ int unencrypt(int edited[]){
 	}
 	return edited;
 }
-int print_histogram(int edited[]){
+void print_histogram(int edited[]){
+	int i = 0, j, counter = 0, char_number[26];
+	double perc[26], max = 0;
+	if (edited == NULL) {
+		printf("Nie je k dispozicii upravena sprava\n");
+		return 0;
+	}
 
+	for (i = 0; i < 26; ++i) {
+		char_number[i] = 0;
+	}
+	i = 0;
+	do {
+		j = edited[i] - 65;
+		if (isalpha(j + 65) && isupper(j + 65)) {
+			++char_number[j];
+			++counter;
+		}
+		++i;
+	} while (isalpha(edited[i]) && isupper(edited[i]));
+
+	for (i = 0; i < 26; ++i) {
+		perc[i] = char_number[i];
+		perc[i] /= counter;
+		perc[i] *= 100;
+		if (perc[i] > max) {
+			max = perc[i];
+		}
+
+	}
+	
+	if (max >= 90) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 90) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+
+	if (max >= 80) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 80) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	if (max >= 70) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 70) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	if (max >= 60) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 60) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+
+	if (max >= 50) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 50) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+
+	if (max >= 40) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 40) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+
+	if (max >= 30) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 30) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}if (max >= 20) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 20) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	
+	if (max >= 10) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 10) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	if (max >= 0) {
+		for (i = 0; i < 26; ++i) {
+			if (perc[i] > 0) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	for (i = 0; i < 26; ++i) {
+		printf("%c ", i + 65);
+	}
+	printf("\n");
+
+	return 0;
 }
 int print_specific_lenght(int original[]){
 
