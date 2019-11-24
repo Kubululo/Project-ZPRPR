00612abf1be9ac4aba2a2ffe1c07a0ce95803c20
Jakub Skurcak <skurcak@outlook.com>
2019-11-24 12:58:14 +0100

    Create first few functions

---
+++ project.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int print_specific_lenght(int original[]);
+int load(int original[]);
+int edit(int original[], int edited[]);
+int print_original( int original[]);
+int print_edited(int edited[]);
+void main(){
+    
+}
+int load( int original[]){
+
+}
+int edit(int original[], int edited[]){
+
+}
+int print_original( int original[]){
+
+}
+int print_edited(int edited[]){
+
+}
+int print_specific_lenght(int original[]){
+
+}
